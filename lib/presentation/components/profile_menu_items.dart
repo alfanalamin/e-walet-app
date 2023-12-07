a718d8d9ac2cd0dcbc45a62026bde39fd39f05f5
alfanalamin <alfanalamin123@gmail.com>
2023-12-07 11:39:09 +0700

    change var ontap

--- lib/presentation/components/profile_menu_items.dart
+++ lib/presentation/components/profile_menu_items.dart
@@ -5,32 +5,32 @@ import 'package:flutter/material.dart';
 class ProfileMenuItem extends StatelessWidget {
   final String title;
   final String iconUrl;
-  final VoidCallback onTap;
+  final VoidCallback? onTap;
 
-  const ProfileMenuItem({
-    Key? key,
-    required this.title,
-    required this.iconUrl,
-    required this.onTap,
-  }) : super(key: key);
+  const ProfileMenuItem(
+      {Key? key, required this.title, required this.iconUrl, this.onTap})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 30),
-      child: Row(
-        children: [
-          Image.asset(
-            iconUrl,
-            width: 24,
-          ),
-          const SizedBox(width: 18),
-          Text(
-            title,
-            style: AppTheme.appTextTheme.regulerNoneReguler!
-                .copyWith(color: BaseColors.neutral900, fontFamily: 'Poppins'),
-          ),
-        ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 30),
+        child: Row(
+          children: [
+            Image.asset(
+              iconUrl,
+              width: 24,
+            ),
+            const SizedBox(width: 18),
+            Text(
+              title,
+              style: AppTheme.appTextTheme.regulerNoneReguler!.copyWith(
+                  color: BaseColors.neutral900, fontFamily: 'Poppins'),
+            ),
+          ],
+        ),
       ),
     );
   }
