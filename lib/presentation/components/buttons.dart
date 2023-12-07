a4d58f7dfa4f2fd00b1ece6c6fe02df777a705d6
alfanalamin <alfanalamin123@gmail.com>
2023-12-07 12:34:46 +0700

    add costum wdgt bt input

--- lib/presentation/components/buttons.dart
+++ lib/presentation/components/buttons.dart
@@ -74,3 +74,36 @@ class CostumFilledText extends StatelessWidget {
     );
   }
 }
+
+class CostumInputButton extends StatelessWidget {
+  final String title;
+  final VoidCallback? onTap;
+  const CostumInputButton({
+    Key? key,
+    required this.title,
+    this.onTap,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 60,
+        height: 60,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: BaseColors.neutral700,
+        ),
+        child: Center(
+          child: Text(
+            title,
+            style: AppTheme.appTextTheme.largeNoneMedium!.copyWith(
+              color: BaseColors.neutral50,
+              fontFamily: 'Poppins',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
