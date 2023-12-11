957fbe8b10f9b2315497a1ff3c22d116b7dc1399
alfanalamin <alfanalamin123@gmail.com>
2023-12-11 22:09:10 +0700

    add method formatCurrency

--- lib/common/shared_methods.dart
+++ lib/common/shared_methods.dart
@@ -1,6 +1,7 @@
 import 'package:another_flushbar/flushbar.dart';
 import 'package:ewallet/common/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void showCostumSnackbar(BuildContext context, String message) {
   Flushbar(
@@ -9,4 +10,15 @@ void showCostumSnackbar(BuildContext context, String message) {
     backgroundColor: BaseColors.danger500,
     duration: const Duration(seconds: 3),
   ).show(context);
+}
+
+String formatCurrency(
+  num number, {
+  String symbol = 'Rp ',
+}) {
+  return NumberFormat.currency(
+    locale: 'id',
+    symbol: symbol,
+    decimalDigits: 0,
+  ).format(number);
 }
